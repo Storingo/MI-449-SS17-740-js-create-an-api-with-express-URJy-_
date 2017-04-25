0ec8ba8a342d42a5896a2695f27a4102833b8110
Dylan O'Dowd <Dylan O'Dowd>
2017-04-25 16:26:42 -0400

    use express to create an api

---
+++ server.js
@@ -0,0 +1,63 @@
+var todos = require('./todo.js')
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+var port = process.env.PORT || 8080
+
+app.get('/', function (request, response) {
+  response.json({
+    welcome: 'welcome to my API!'
+  })
+})
+
+app.get('/todos', function (request, response) {
+  response.json(todos)
+})
+
+app.get('/todos/:slug', function (request, response) {
+  if (!todos[request.params.slug]) {
+    response.status(404).end('sorry, no todo: ' + request.params.slug)
+    return
+  }
+  response.json(todos[request.params.slug])
+})
+
+app.post('/todos', function (request, response) {
+  var slug = request.body.text.trim().toLowerCase().split(' ').join('-')
+  todos[slug] = {
+    text: request.body.text.trim(),
+    completed: request.body.completed.toUpperCase()
+  }
+  response.redirect('/todos/' + slug)
+})
+
+app.delete('/todos/:slug', function (request, response) {
+  if (!todos[request.params.slug]) {
+    response.status(404).end('sorry, no todo item: ' + request.params.slug)
+    return
+  }
+  delete todos[request.params.slug]
+  response.redirect('/todos')
+})
+
+app.put('/todos/:slug', function (request, response) {
+  if (!todos[request.params.slug]) {
+    response.status(404).end('sorry, no todo item: ' + request.params.slug)
+    return
+  }
+  var todo = todos[request.params.slug]
+  if (request.body.text !== undefined) {
+    todo.text = request.body.text.trim()
+  }
+  if (request.body.completed !== undefined) {
+    todo.completed = request.body.completed.toUpperCase()
+  }
+  response.redirect('/todos')
+})
+
+app.use(function (request, response, next) {
+  response.status(404).end(request.url + ' not found')
+})
+
+app.listen(port)
---
+++ todo.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'grocery-shopping': {
+    text: 'Grocery Shopping',
+    completed: 'YES'
+  },
+  'do-homework': {
+    text: 'Do homework',
+    completed: 'NO'
+  }
+}
